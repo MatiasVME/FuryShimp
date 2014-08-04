b48a6444cabdca4b0c3461fe157794e7f78d437d
Matías Muñoz Espinoza <writkas2@gmail.com>
2014-08-04 01:32:23 -0400

    Init

---
+++ android/src/org/furygames/furyshimp/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package org.furygames.furyshimp.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import org.furygames.furyshimp.FuryShimp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FuryShimp(), config);
+	}
+}
---
+++ desktop/src/org/furygames/furyshimp/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.furygames.furyshimp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.furygames.furyshimp.FuryShimp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FuryShimp(), config);
+	}
+}
