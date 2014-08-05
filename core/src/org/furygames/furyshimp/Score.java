2544b2ec7981d9e7372772c1857a285453c58fce
victor <victorgraciaweb@gmail.com>
2014-08-05 19:08:32 +0200

    cambios

--- core/src/org/furygames/furyshimp/Score.java
+++ core/src/org/furygames/furyshimp/Score.java
@@ -1,22 +1,22 @@
 package org.furygames.furyshimp;
 
 public class Score {
-	private int badScore = 0;
-	private int goodScore = 0;
+	private static int badScore = 0;
+	private static int goodScore = 0;
 	
 	public int getBadScore() {
 		return badScore;
 	}
 	
-	public void setBadScore(int badScore) {
-		this.badScore = badScore;
+	public static void setBadScore(int badScore) {
+		Score.badScore = badScore;
 	}
 	
 	public int getGoodScore() {
 		return goodScore;
 	}
 	
-	public void setGoodScore(int goodScore) {
-		this.goodScore = goodScore;
+	public static void setGoodScore(int goodScore) {
+		Score.goodScore = goodScore;
 	}
 }
