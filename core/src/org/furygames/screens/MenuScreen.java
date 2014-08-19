fc8130c03624228126e60249f167bb1404a4ebaa
Matías Muñoz Espinoza <writkas2@gmail.com>
2014-08-19 12:40:17 -0400

    cambio el menu por imagenes

--- core/src/org/furygames/screens/MenuScreen.java
+++ core/src/org/furygames/screens/MenuScreen.java
@@ -6,44 +6,47 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class MenuScreen extends GenericScreen {
-
-	private Skin skin;
-	private Table table;
-	private TextButton startButton;
-	private TextButton creditButton;
-	private TextButton exitButton;
+	
+	private Texture ttStartButton;
+	private Texture ttCreditButton;
+	private Texture ttExitButton;
+	
+	private Image startButton;
+	private Image creditButton;
+	private Image exitButton;
 	
 	private static Music music = null;
 
 	public MenuScreen (final FuryShimp universalMonkey) {
 		super(universalMonkey);
 		
-		skin = new Skin(Gdx.files.internal("uiskin/uiskin.json"));
 		music = Gdx.audio.newMusic(Gdx.files.internal("sounds/Salt Marsh Birds.mp3"));
-
-		startButton = new TextButton("Start", skin);
-		creditButton = new TextButton("Credits", skin);
-		exitButton = new TextButton("Exit", skin);
 		
-		final float pad = 40;
-		final float width = 400f;
-		final float height = 100f;
+		ttStartButton = new Texture ("buttons/start_button.png");
+		ttCreditButton = new Texture ("buttons/credit_button.png");
+		ttExitButton = new Texture ("buttons/exit_button.png");
 
-		table = new Table();
-		table.setFillParent(true);
-		table.add(startButton).pad(pad).width(width).height(height);
-		table.row();
-		table.add(creditButton).pad(pad).width(width).height(height);
-		table.row();
-		table.add(exitButton).pad(pad).width(width).height(height);
-
+		startButton = new Image(ttStartButton);
+		creditButton = new Image(ttCreditButton);
+		exitButton = new Image(ttExitButton);
+		
+		startButton.setSize(256f, 128f);
+		creditButton.setSize(256f, 128f);
+		exitButton.setSize(256f, 128f);		
+		
+		startButton.setCenterPosition(WIDTH / 2, 600f);
+		creditButton.setCenterPosition(WIDTH / 2, 400f);
+		exitButton.setCenterPosition(WIDTH / 2, 200f);
+	
 		startButton.addListener(new ClickListener(){
 			@Override 
 			public void clicked(InputEvent event, float x, float y){
@@ -65,7 +68,9 @@ public class MenuScreen extends GenericScreen {
 			}
 		});
 
-		stage.addActor(table);
+		stage.addActor(startButton);
+		stage.addActor(creditButton);
+		stage.addActor(exitButton);
 	}
 
 	@Override
