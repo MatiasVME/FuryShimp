046d698317f5108637cf7cff49d2d76c767cff32
Matías Muñoz Espinoza <writkas2@gmail.com>
2014-08-19 12:05:31 -0400

    añado algunas imagenes

--- core/src/org/furygames/furyshimp/BackgroundManager.java
+++ core/src/org/furygames/furyshimp/BackgroundManager.java
@@ -9,7 +9,7 @@ public class BackgroundManager {
 	
 	private final float WIDTH = 1280f;
 	private final float HEIGHT = 720f;
-	private final int TOTAL_BACKGROUNDS = 3;
+	private final int TOTAL_BACKGROUNDS = 4;
 		
 	private Texture [] ttBackgrounds;	
 	private Image [] imgBackgrounds;
@@ -22,36 +22,41 @@ public class BackgroundManager {
 		ttBackgrounds[0] = new Texture ("backgrounds/bgjungle.jpg");
 		ttBackgrounds[1] = new Texture ("backgrounds/bgbeach.jpg");
 		ttBackgrounds[2] = new Texture ("backgrounds/bgmountains.jpg");
+		ttBackgrounds[3] = new Texture ("backgrounds/bgbeach2.jpg");
 		
 		// Inicializar actores img
 		imgBackgrounds[0] = new Image(ttBackgrounds[0]);
 		imgBackgrounds[1] = new Image(ttBackgrounds[1]);
 		imgBackgrounds[2] = new Image(ttBackgrounds[2]);
+		imgBackgrounds[3] = new Image(ttBackgrounds[3]);
 		
 		// Le damos tamaño a los actores
 		imgBackgrounds[0].setSize(WIDTH, HEIGHT);
 		imgBackgrounds[1].setSize(WIDTH, HEIGHT);
 		imgBackgrounds[2].setSize(WIDTH, HEIGHT);
+		imgBackgrounds[3].setSize(WIDTH, HEIGHT);
 		
 		// Le damos posición a los actores
 		imgBackgrounds[0].setCenterPosition(GameScreen.WIDTH / 2, GameScreen.HEIGHT / 2);
 		imgBackgrounds[1].setCenterPosition(GameScreen.WIDTH / 2, GameScreen.HEIGHT / 2);
 		imgBackgrounds[2].setCenterPosition(GameScreen.WIDTH / 2, GameScreen.HEIGHT / 2);
-		
-	
+		imgBackgrounds[3].setCenterPosition(GameScreen.WIDTH / 2, GameScreen.HEIGHT / 2);
 	}
 	
 	public Image getImage(int img) {
 		switch (img) {
-		case 1:
-			return imgBackgrounds[0];
-		case 2:
-			return imgBackgrounds[1];
-		case 3:
-			return imgBackgrounds[2];
-		default:
-			break;
+			case 1:
+				return imgBackgrounds[0];
+			case 2:
+				return imgBackgrounds[1];
+			case 3:
+				return imgBackgrounds[2];
+			case 4:
+				return imgBackgrounds[3];
+			default:
+				break;
 		}
+		
 		return null;
 	}
 }
